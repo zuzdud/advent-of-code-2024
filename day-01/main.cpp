1432f4324b1adfa5b1d49479d607354abbfb05a4
zuzuz <duda.zuzanna7@gmail.com>
2024-12-01 15:27:40 +0100

    day 1 - second task nice and working

--- day-01/main.cpp
+++ day-01/main.cpp
@@ -1,12 +1,14 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <sstream>
 #include <vector>
 
 
 std::vector<int> firstVector;
 std::vector<int> secondVector;
+std::map<int, int> dict;
 
 // first task
 void readInputFileFirstTask(const std::string &inputFileName) {
@@ -24,6 +26,30 @@ void readInputFileFirstTask(const std::string &inputFileName) {
     }
 }
 
+// second task
+void readInputFileSecondTask(const std::string &inputFileName) {
+    if (std::ifstream inputFile(inputFileName); inputFile.is_open()) {
+        int secondNumber;
+        int firstNumber;
+        std::string line;
+        while (getline(inputFile, line)) {
+            std::stringstream sstream(line);
+            sstream >> firstNumber >> secondNumber;
+
+            firstVector.push_back(firstNumber);
+            dict[secondNumber] = dict[secondNumber] + 1;
+        }
+    }
+}
+
+int getResultSecondTask() {
+    int result = 0;
+    for (int i : firstVector) {
+        result += i * dict[i];
+    }
+    return result;
+}
+
 int getResultsFirstTask() {
     int result = 0;
     std::sort(firstVector.begin(), firstVector.end());
@@ -39,6 +65,10 @@ int main() {
     // first task
     readInputFileFirstTask("C:/Users/zuzuz/CLionProjects/advent-of-code-2024/day-01/myInput.txt");
     std::cout << getResultsFirstTask() << std::endl;
+
+    // second task
+    readInputFileSecondTask("C:/Users/zuzuz/CLionProjects/advent-of-code-2024/day-01/myInput.txt");
+    std::cout << getResultSecondTask() << std::endl;
 
     return 0;
 }
