c87e5236c70b8e0363d4654e99da9aa758eede53
zuzuz <duda.zuzanna7@gmail.com>
2024-12-11 19:50:56 +0100

    day 06 - second part works but takes few minutes

--- day-06/main.cpp
+++ day-06/main.cpp
@@ -24,7 +24,7 @@ void findObstacles(const Matrix &map, std::vector<std::pair<int, int> > &coordin
         const std::string &line = map[i];
         for (int j = 0; j < line.size(); j++) {
             if (line[j] == '#') {
-                coordinates.push_back(std::pair(i, j));
+                coordinates.emplace_back(i, j);
             }
             if (line[j] == '^') {
                 guardCoordinates = std::pair(i, j);
@@ -74,6 +74,58 @@ std::set<std::pair<int, int> > findPatrolPath(std::pair<int, int> &guardCoordina
     return patrolPath;
 }
 
+std::set<std::pair<int, int> > findLoops(std::pair<int, int> &guardCoordinates,
+                                         const std::vector<std::pair<int, int> > &obstaclesCoordinates,
+                                         const int maxLine, const int maxPosition, bool &loop) {
+    std::set<std::pair<int, int> > patrolPath;
+
+    std::vector<std::pair<std::pair<int, int>, std::pair<int, int> > > steps;
+    char direction = 'U';
+    loop = false;
+    while (guardCoordinates.first >= 0 && guardCoordinates.first < maxLine && guardCoordinates.second >= 0 &&
+           guardCoordinates.second < maxPosition) {
+        patrolPath.insert(guardCoordinates);
+        std::pair root(guardCoordinates);
+
+        if (direction == 'U') {
+            guardCoordinates.first = guardCoordinates.first - 1;
+        } else if (direction == 'D') {
+            guardCoordinates.first = guardCoordinates.first + 1;
+        } else if (direction == 'R') {
+            guardCoordinates.second = guardCoordinates.second + 1;
+        } else {
+            guardCoordinates.second = guardCoordinates.second - 1;
+        }
+
+        if (std::find(obstaclesCoordinates.begin(), obstaclesCoordinates.end(), guardCoordinates) !=
+            obstaclesCoordinates.end()) {
+            if (direction == 'U') {
+                guardCoordinates.first = guardCoordinates.first + 1;
+                direction = 'R';
+            } else if (direction == 'D') {
+                guardCoordinates.first = guardCoordinates.first - 1;
+                direction = 'L';
+            } else if (direction == 'R') {
+                guardCoordinates.second = guardCoordinates.second - 1;
+                direction = 'D';
+            } else {
+                guardCoordinates.second = guardCoordinates.second + 1;
+                direction = 'U';
+            }
+        } else {
+            std::pair next(guardCoordinates);
+
+            if (std::find(steps.begin(), steps.end(), std::pair(root, next)) != steps.end()) {
+                loop = true;
+                break;
+            }
+            steps.emplace_back(root, next);
+        }
+    }
+
+    return patrolPath;
+}
+
 void firstTask(const std::string &inputFileName) {
     const Matrix map = getMap(inputFileName);
 
@@ -87,8 +139,42 @@ void firstTask(const std::string &inputFileName) {
     std::cout << patrolPath.size() << std::endl;
 }
 
+void secondTask(const std::string &inputFileName) {
+    const Matrix map = getMap(inputFileName);
+
+    std::vector<std::pair<int, int> > originalObstaclesCoordinates;
+    std::pair<int, int> originalGuardCoordinates;
+
+    findObstacles(map, originalObstaclesCoordinates, originalGuardCoordinates);
+
+    const std::vector<std::pair<int, int> > obstaclesCoordinates = originalObstaclesCoordinates;
+    const std::pair<int, int> guardCoordinates = originalGuardCoordinates;
+
+    const std::set<std::pair<int, int> > patrolPath = findPatrolPath(originalGuardCoordinates,
+                                                                     originalObstaclesCoordinates, map.size(),
+                                                                     map[0].size());
+
+    int loops = 0;
+    for (auto pair: patrolPath) {
+        std::vector<std::pair<int, int> > newObstacles = obstaclesCoordinates;
+        std::pair<int, int> newGuard = guardCoordinates;
+        newObstacles.push_back(pair);
+        bool isloop = false;
+        const std::set<std::pair<int, int> > newPatrolPath = findLoops(
+            newGuard, newObstacles, map.size(),
+            map[0].size(), isloop);
+        if (isloop) {
+            loops++;
+        }
+    }
+
+    std::cout << loops << std::endl;
+}
+
+
 int main() {
     std::cout << "Advent of Code 2024 - Day 06" << std::endl;
     firstTask("C:/Users/zuzuz/CLionProjects/advent-of-code-2024/day-06/myInput.txt");
+    secondTask("C:/Users/zuzuz/CLionProjects/advent-of-code-2024/day-06/myInput.txt");
     return 0;
 }
