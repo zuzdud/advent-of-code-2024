68d8ff7bf6b86e39a48475d567b17477986bbfe1
zuzuz <duda.zuzanna7@gmail.com>
2024-12-11 16:34:59 +0100

    day 11 - first task done

--- day-11/main.cpp
+++ day-11/main.cpp
@@ -1,7 +1,55 @@
+#include <fstream>
 #include <iostream>
 #include <ostream>
+#include <sstream>
+#include <vector>
+#include <bits/stdc++.h>
+
+std::vector<long long> getInitialArrangement(const std::string &inputFileName) {
+    std::vector<long long> initialArrangement;
+    if (std::ifstream inputFile(inputFileName); inputFile.is_open()) {
+        std::string line;
+        long long stone;
+        getline(inputFile, line);
+        std::stringstream sstream(line);
+        while (sstream.rdbuf()->in_avail() != 0) {
+            sstream >> stone;
+            initialArrangement.push_back(stone);
+        }
+    }
+    return initialArrangement;
+}
+
+int countDigits(const long long n) { return floor(log10(n) + 1); }
+
+std::vector<long long> rearrangeStones(const std::vector<long long> &initialArrangement) {
+    std::vector<long long> newArrangement;
+    for (const auto i: initialArrangement) {
+        if (i == 0) {
+            newArrangement.push_back(1);
+        } else if (countDigits(i) % 2 == 0) {
+            std::string s = std::to_string(i);
+            long long firstStone = std::stoll(s.substr(0, countDigits(i) / 2));
+            long long secondStone = std::stoll(s.substr(countDigits(i) / 2, countDigits(i) / 2));
+            newArrangement.push_back(firstStone);
+            newArrangement.push_back(secondStone);
+        } else {
+            newArrangement.push_back(i * 2024);
+        }
+    }
+    return newArrangement;
+}
+
+void firstTask(const std::string &inputFileName) {
+    std::vector<long long> initialVector = getInitialArrangement(inputFileName);
+    for (int i = 0; i < 25; i++) {
+        initialVector = rearrangeStones(initialVector);
+    }
+    std::cout << initialVector.size();
+}
 
 int main() {
     std::cout << "Advent of Code 2024 - Day 11" << std::endl;
+    firstTask("C:/Users/zuzuz/CLionProjects/advent-of-code-2024/day-11/myInput.txt");
     return 0;
 }
