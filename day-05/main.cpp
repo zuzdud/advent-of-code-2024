a900fd9bace5c817bfccc08e054d298ce469e223
zuzuz <duda.zuzanna7@gmail.com>
2025-01-10 15:39:39 +0100

    day 05 - second part done

--- day-05/main.cpp
+++ day-05/main.cpp
@@ -45,6 +45,22 @@ bool isCorrectlyOrdered(const std::vector<int> &update, std::unordered_map<int, 
     return true;
 }
 
+void changeOrder(std::vector<int> &update, std::unordered_map<int, std::vector<int> > &rules) {
+    for (int i = 1; i < update.size(); i++) {
+        int key = update[i];
+        std::vector<int> rule = rules[key];
+
+        for (int j = 0; j < i; j++) {
+            if (std::find(rule.begin(), rule.end(), update[j]) != rule.end()) {
+                const int temp = update[j];
+                update[j] = key;
+                update[i] = temp;
+                break;
+            }
+        }
+    }
+}
+
 void firstPart(const std::string &inputFileName) {
     std::unordered_map<int, std::vector<int> > rules;
     std::vector<std::vector<int> > pagesToProduce;
@@ -59,8 +75,26 @@ void firstPart(const std::string &inputFileName) {
     std::cout << sum << std::endl;
 }
 
+void secondPart(const std::string &inputFileName) {
+    std::unordered_map<int, std::vector<int> > rules;
+    std::vector<std::vector<int> > pagesToProduce;
+    getPageOrderingRules(inputFileName, rules, pagesToProduce);
+
+    int sum = 0;
+    for (auto &update: pagesToProduce) {
+        if (!isCorrectlyOrdered(update, rules)) {
+            while(!isCorrectlyOrdered(update, rules)) {
+                changeOrder(update, rules);
+            }
+            sum += update[update.size() / 2];
+        }
+    }
+    std::cout << sum << std::endl;
+}
+
 int main() {
     std::cout << "Advent of Code 2024 - Day 05" << std::endl;
     firstPart("C:/Users/zuzuz/CLionProjects/advent-of-code-2024/day-05/myInput.txt");
+    secondPart("C:/Users/zuzuz/CLionProjects/advent-of-code-2024/day-05/myInput.txt");
     return 0;
 }
